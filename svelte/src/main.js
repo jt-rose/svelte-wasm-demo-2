9078c077f59c9545f35e5ec37e3a207e62672dc5
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-12-16 00:25:05 -0500

    move wasm files into frontend src

--- svelte/src/main.js
+++ svelte/src/main.js
@@ -1,5 +1,6 @@
 import App from "./App.svelte";
-import wasm from "../../rust/Cargo.toml";
+import wasm from "./rust/Cargo.toml";
+//import wasm from "./pkg/rust";
 
 const init = async () => {
   const bindings = await wasm();
